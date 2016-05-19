6e683dd0433f7aa7f0c4e4a15257c3edcdf88189
Dhi Aurrahman <dio@hooq.tv>
2016-05-19 07:59:31 +0700

    Add more test on extended error

--- app/templates/test.js
+++ app/templates/test.js
@@ -1,7 +1,16 @@
 import test from 'ava';
 import <%= className %> from '../lib';
+import { <%= className %>Error } from '../lib';
 
 test('Yay!', async (t) => {
     const instance = new <%= className %>('HOOQ');
     t.deepEqual(await instance.hello(), 'Hello HOOQ');
+});
+
+test('Nay!', async (t) => {
+    const err = new <%= className %>Error('Nay!', 500, 'Dragon');
+    t.true(err instanceof Error);
+    t.true(err instanceof <%= className %>Error);
+    t.deepEqual(err.code, 500);
+    t.deepEqual(err.message, 'Nay! (500 of Dragon)');
 });
